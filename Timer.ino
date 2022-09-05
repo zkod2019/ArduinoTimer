d3ec8ad246ce806964724033009edafc09cf847d
ZKod <zkod777@gmail.com>
2022-09-05 14:07:33 +0400

    values set in 2D array

--- Timer.ino
+++ Timer.ino
@@ -1,5 +1,3 @@
-
-
 int pinA = 2;
 int pinB = 3;
 int pinC = 4;
@@ -11,6 +9,52 @@ int D1 = 10;
 int D2 = 11;
 int D3 = 12;
 int D4 = 13;
+
+static int mapping[10][7] = {
+  {LOW, LOW, LOW, LOW, LOW, LOW, HIGH}, // 0
+  {HIGH, LOW, LOW, HIGH, HIGH, HIGH, HIGH}, // 1
+  {LOW, LOW, HIGH, LOW, LOW, HIGH, LOW}, // 2
+  {LOW, LOW, LOW, LOW, HIGH, HIGH, HIGH}, // 3
+  {HIGH, LOW, LOW, HIGH, HIGH, LOW, LOW}, // 4
+  {LOW, HIGH, LOW, LOW, HIGH, LOW, LOW}, // 5
+  {LOW, HIGH, LOW, LOW, LOW, LOW, LOW}, // 6
+  {LOW, LOW, LOW, HIGH, HIGH, HIGH, HIGH}, // 7
+  {LOW, LOW, LOW, LOW, LOW, LOW, LOW}, // 8
+  {LOW, LOW, LOW, LOW, HIGH, LOW, LOW}, // 9
+};
+
+void writeDigit(int number, int idx) {
+  int* segs = mapping[number];
+  digitalWrite(pinA, segs[0]);
+  digitalWrite(pinB, segs[1]);   
+  digitalWrite(pinC, segs[2]);
+  digitalWrite(pinD, segs[3]);
+  digitalWrite(pinE, segs[4]);
+  digitalWrite(pinF, segs[5]);
+  digitalWrite(pinG, segs[6]);
+
+  if (idx == 0) {
+    digitalWrite(D1, HIGH);
+    digitalWrite(D2, LOW);
+    digitalWrite(D3, LOW);
+    digitalWrite(D4, LOW);
+  } else if (idx == 1) {
+    digitalWrite(D2, HIGH);
+    digitalWrite(D1, LOW);
+    digitalWrite(D3, LOW);
+    digitalWrite(D4, LOW);
+  } else if (idx == 2) {
+    digitalWrite(D3, HIGH);
+    digitalWrite(D1, LOW);
+    digitalWrite(D2, LOW);
+    digitalWrite(D4, LOW);
+  } else if (idx == 3) {
+    digitalWrite(D4, HIGH);
+    digitalWrite(D1, LOW);
+    digitalWrite(D2, LOW);
+    digitalWrite(D3, LOW);
+  }
+}
 
 // the setup routine runs once when you press reset:
 void setup() {  
@@ -25,133 +69,18 @@ void setup() {
   pinMode(pinF, OUTPUT);     
   pinMode(pinG, OUTPUT);   
   pinMode(D1, OUTPUT);  
-  pinMode(D2, OUTPUT);  
-  pinMode(D3, OUTPUT);  
-  pinMode(D4, OUTPUT);  
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
 }
 
+int digit = 0;
 // the loop routine runs over and over again forever:
 void loop() {
-  Serial.print("Enter time (min:sec): ");
-
-  while (Serial.available() == 0)
-    ;
-  int timer = Serial.parseInt();
-  Serial.print(timer);
-  digitalWrite(D1, HIGH);
-  digitalWrite(D2, HIGH);
-  digitalWrite(D3, HIGH);
-  digitalWrite(D4, HIGH); 
-  //0
-  digitalWrite(pinA, LOW);   
-  digitalWrite(pinB, LOW);   
-  digitalWrite(pinC, LOW);   
-  digitalWrite(pinD, LOW);   
-  digitalWrite(pinE, LOW);   
-  digitalWrite(pinF, LOW);   
-  digitalWrite(pinG, HIGH);   
-  delay(1);               // wait for a second
-  /*
-  digitalWrite(D1, LOW);
-  digitalWrite(D2, HIGH);
-  digitalWrite(D3, LOW);
-  digitalWrite(D4, LOW); 
-  //1
-  digitalWrite(pinA, LOW);   
-  digitalWrite(pinB, LOW);   
-  digitalWrite(pinC, LOW);   
-  digitalWrite(pinD, HIGH);   
-  digitalWrite(pinE, LOW);   
-  digitalWrite(pinF, LOW);   
-  digitalWrite(pinG, LOW);   
-  delay(1);               // wait for a second
-  
-  digitalWrite(D1, LOW);
-  digitalWrite(D2, LOW);
-  digitalWrite(D3, HIGH);
-  digitalWrite(D4, LOW); 
-  //2
-  digitalWrite(pinA, HIGH);   
-  digitalWrite(pinB, HIGH);   
-  digitalWrite(pinC, LOW);   
-  digitalWrite(pinD, HIGH);   
-  digitalWrite(pinE, LOW);   
-  digitalWrite(pinF, HIGH);   
-  digitalWrite(pinG, LOW);     
-  delay(1);               // wait for a second
-
-  digitalWrite(D1, LOW);
-  digitalWrite(D2, LOW);
-  digitalWrite(D3, LOW);
-  digitalWrite(D4, HIGH); 
-  //3
-  digitalWrite(pinA, LOW);   
-  digitalWrite(pinB, HIGH);   
-  digitalWrite(pinC, LOW);   
-  digitalWrite(pinD, HIGH);   
-  digitalWrite(pinE, LOW);   
-  digitalWrite(pinF, LOW);   
-  digitalWrite(pinG, HIGH);     
-  delay(1);               // wait for a second
-  
-  //4
-  digitalWrite(pinA, HIGH);   
-  digitalWrite(pinB, LOW);   
-  digitalWrite(pinC, LOW);   
-  digitalWrite(pinD, HIGH);   
-  digitalWrite(pinE, HIGH);   
-  digitalWrite(pinF, LOW);   
-  digitalWrite(pinG, LOW);     
-  delay(1000);               // wait for a second
-  
-  //5
-  digitalWrite(pinA, LOW);   
-  digitalWrite(pinB, HIGH);   
-  digitalWrite(pinC, LOW);   
-  digitalWrite(pinD, LOW);   
-  digitalWrite(pinE, HIGH);   
-  digitalWrite(pinF, LOW);   
-  digitalWrite(pinG, LOW);     
-  delay(1000);               // wait for a second
-  
-  //6
-  digitalWrite(pinA, LOW);   
-  digitalWrite(pinB, HIGH);   
-  digitalWrite(pinC, LOW);   
-  digitalWrite(pinD, LOW);   
-  digitalWrite(pinE, LOW);   
-  digitalWrite(pinF, LOW);   
-  digitalWrite(pinG, LOW);     
-  delay(1000);               // wait for a second
-  
-  //7
-  digitalWrite(pinA, LOW);   
-  digitalWrite(pinB, LOW);   
-  digitalWrite(pinC, LOW);   
-  digitalWrite(pinD, HIGH);   
-  digitalWrite(pinE, HIGH);   
-  digitalWrite(pinF, HIGH);   
-  digitalWrite(pinG, HIGH);     
-  delay(1000);               // wait for a second
-  
-  //8
-  digitalWrite(pinA, HIGH);   
-  digitalWrite(pinB, HIGH);   
-  digitalWrite(pinC, HIGH);   
-  digitalWrite(pinD, HIGH);   
-  digitalWrite(pinE, HIGH);   
-  digitalWrite(pinF, HIGH);   
-  digitalWrite(pinG, HIGH);     
-  delay(1000);               // wait for a second
-
-  //9
-  digitalWrite(pinA, HIGH);   
-  digitalWrite(pinB, HIGH);   
-  digitalWrite(pinC, HIGH);   
-  digitalWrite(pinD, HIGH);   
-  digitalWrite(pinE, HIGH);   
-  digitalWrite(pinF, HIGH);   
-  digitalWrite(pinG, HIGH);     
-  delay(1000);               // wait for a second
-  */
+  writeDigit(digit, 0);
+  writeDigit(digit, 1);
+  writeDigit(digit, 2);
+  writeDigit(digit, 3);
+  digit = digit + 1 % 9;
+  delay(2000);
 }
